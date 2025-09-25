4f10ccc96fc1f272493197170a3a3d56aacea6a8
github-felipe <felipe.lima.eu@hotmail.com>
2025-09-25 18:55:42 -0300

    Agora sim Adicionando o arquivo referente a aula sobre funções

--- aulaFuncoes/funcoes.go
+++ aulaFuncoes/funcoes.go
@@ -1,1 +1,34 @@
-package aulaFuncoes
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(soma(42, 13))
+	fmt.Println(subtracao(42, 13))
+
+	nome1, nome2, _ := retornaNome("Felipe")
+	fmt.Println(nome1)
+	fmt.Println(nome2)
+
+	nome3, _, _ := retornaNome("felipe")
+	fmt.Println(nome3)
+
+	nomeCompleto := retornaNomeCompleto("Felipe", "Lima")
+	fmt.Println(nomeCompleto)
+}
+
+func soma(x int, y int) int {
+	return x + y
+}
+
+func subtracao(x int, y int) int {
+	return x - y
+}
+
+func retornaNome(nome string) (string, string, string) {
+	return nome, nome, nome
+}
+
+func retornaNomeCompleto(nome, sobrenome string) string {
+	return nome + " " + sobrenome
+}
